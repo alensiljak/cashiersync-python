8be320ccecc1ca1673651b68bc06fed7bfdf8ac3
Alen Šiljak <alen@alensiljak.eu.org>
2020-05-07 11:46:11 +0200

    v1.1.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from os import path
 setup(
     name="cashiersync",
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    version='1.0.2',
+    version='1.1.0',
     description="Server-side synchronization component for Cashier",
     author="Alen Siljak",
     author_email="cashier@alensiljak.eu.org",
